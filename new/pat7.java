8eaac3a41512f70e27fba2ed030df3479df21d60
paraggarg17 <paraggarg1718@gmail.com>
2024-11-26 22:49:57 +0530

    ch

---
+++ new/pat7.java
@@ -0,0 +1,10 @@
+public class pat7 {
+    public static void main(String[] args) {
+        for(int i=1;i<=4;i++){
+            for(int j=i;j<=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
