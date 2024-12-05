c317837becf8224ec0c65a4a43b0a2a70bdfea19
paraggarg17 <paraggarg1718@gmail.com>
2024-12-05 23:44:41 +0530

    gffg

---
+++ new/function/pat9.java
@@ -0,0 +1,22 @@
+package function;
+
+public class pat9 {
+    public static void inverted_rotated(int n,int m){
+        for(int i =1; i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for( int j=1;j<=i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        inverted_rotated(4, 4);
+        
+    }
+    
+}
