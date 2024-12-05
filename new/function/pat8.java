46ce28739cb8fc8165123b41e6cb208dd346cffd
paraggarg17 <paraggarg1718@gmail.com>
2024-12-05 20:25:21 +0530

    patta

--- new/function/pat8.java
+++ new/function/pat8.java
@@ -1,10 +1,24 @@
 package function;
 
 public class pat8 {
-    public static void main(String[] args) {
-        for(int i =1;i<=5;i++){
+    public static void hollow_rectangle(int n,int m){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1||i==n||j==1||j==m){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
             
 
         }
+    }
+    public static void main(String[] args) {
+        hollow_rectangle(4, 5);
+        
     }
 }
