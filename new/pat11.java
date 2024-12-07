bb70b60494ec3d101be627d276fff34f14200c13
paraggarg17 <paraggarg1718@gmail.com>
2024-12-07 21:23:05 +0530

    vauh

--- new/pat11.java
+++ new/pat11.java
@@ -3,6 +3,7 @@ public class pat11 {
         for(int i =1;i<=n;i++){
             for(int j=1;j<=m;j++){
                 if(i==1||i==n||j==1||j==m){
+                    System.out.print("#");
                     
                 }
                 
@@ -16,6 +17,7 @@ public class pat11 {
         
     }
     public static void main(String[] args) {
+        hall(4, 5);
         
     }
 }
