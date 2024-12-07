7538a46cafb1671e606c91347fd23ec86b77fd39
paraggarg17 <paraggarg1718@gmail.com>
2024-12-07 21:20:40 +0530

    whj

---
+++ new/pat11.java
@@ -0,0 +1,21 @@
+public class pat11 {
+    public static void hall(int n ,int m){
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(i==1||i==n||j==1||j==m){
+                    
+                }
+                
+                else{
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+}
