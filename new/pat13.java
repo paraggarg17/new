889e4cde29ade1910909f3055f9a0a91713a58f0
paraggarg17 <paraggarg1718@gmail.com>
2024-12-09 00:09:32 +0530

    edhdjshd

---
+++ new/pat13.java
@@ -0,0 +1,22 @@
+public class pat13 {
+    public static void solid(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+
+            }
+            System.out.println();
+            
+        }
+        
+
+        
+    }
+    public static void main(String[] args) {
+        solid(5);
+        
+    }
+}
