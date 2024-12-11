a5da4554284a746ef0b925437323b00ebf80572f
paraggarg17 <paraggarg1718@gmail.com>
2024-12-11 17:49:54 +0530

    djifj

--- new/pat15.java
+++ new/pat15.java
@@ -1,3 +1,17 @@
 public class pat15 {
+    public static void numb(int n) {
+        for(int i=1;i<=n;i++){
+            for( int j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+            
+        
+        }
+    }
+    public static void main(String[] args) {
+        numb(4);
+        
+    }
     
 }
