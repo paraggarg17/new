9539761a11cacba885a0f8c94477bc0926287db5
paraggarg17 <paraggarg1718@gmail.com>
2024-12-12 18:08:11 +0530

    uhiok

--- new/pat16.java
+++ new/pat16.java
@@ -1,3 +1,18 @@
 public class pat16 {
+    public static void coll(int n) {
+        char ch ='A';
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+
+        }
+    }
+    public static void main(String[] args) {
+        coll(4);
+    }
+   
     
 }
