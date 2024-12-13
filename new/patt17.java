9e9ac20c92ed64a23154cb21d3ba50485a6cc33d
paraggarg17 <paraggarg1718@gmail.com>
2024-12-13 22:42:09 +0530

    edkd

--- new/patt17.java
+++ new/patt17.java
@@ -1,3 +1,16 @@
 public class patt17 {
+    public static void cl(int n){
+        for( int i=1;i<=n;i++){
+            for( int j=1;j<=n-i+1;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        cl(5);
+        
+    }
     
 }
