f9e4d6ff5a1271e1c7f5c6edc39a924b3e0bd5f3
paraggarg17 <paraggarg1718@gmail.com>
2024-12-14 22:10:29 +0530

    jhnj

--- new/pat18.java
+++ new/pat18.java
@@ -1,3 +1,16 @@
 public class pat18 {
+    public static void hop(int n) {
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+    }
+    public static void main(String[] args) {
+        hop(4);
+        
+    }
     
 }
