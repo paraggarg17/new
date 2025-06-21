9713b52b90e9b2b220f52b917634996c866ee66d
paraggarg17 <paraggarg1718@gmail.com>
2025-06-21 15:27:40 +0530

    java array program

--- new/function/arr.java
+++ new/function/arr.java
@@ -1,13 +1,20 @@
 package function;
 
 public class arr {
+    public static void main(String[] args) {
+        
+    
     int[] marks = new int[30];
     marks[0]=98;
     marks[1]=97;
     marks[2]=78;
-    system.out.println(marks[0]);
-    system.out.println(marks[1]);
-    system.out.println(marks[2]);
+    //System.out.println(marks[0]);
+     //System.out.println(marks[1]);
+      //  System.out.println(marks[2]);
+    for(int i=0;i<3;i++){
+        System.out.println(marks[i]);
+    }
+    }
 
     
 }
