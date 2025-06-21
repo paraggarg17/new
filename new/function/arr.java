909e46ae1021940599c6909ead4388663ef12541
paraggarg17 <paraggarg1718@gmail.com>
2025-06-21 14:01:13 +0530

    igd

---
+++ new/function/arr.java
@@ -0,0 +1,13 @@
+package function;
+
+public class arr {
+    int[] marks = new int[30];
+    marks[0]=98;
+    marks[1]=97;
+    marks[2]=78;
+    system.out.println(marks[0]);
+    system.out.println(marks[1]);
+    system.out.println(marks[2]);
+
+    
+}
