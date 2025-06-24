04211c3a63beb7629d8e0239258f589e4852f240
paraggarg17 <paraggarg1718@gmail.com>
2025-06-24 15:10:01 +0530

    2d

--- new/function/2darr1.java
+++ new/function/2darr1.java
@@ -1,5 +1,34 @@
 package function;
 
 public class 2darr1 {
+    public static void main(string[]arr){
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.newInt();
+        int cols = sc.newInt();
+
+        int[][] number = new int [rows][cols];
+        //input
+        for( int i=0;i<rows;i++){
+            for(int  j=0; j<cols; j++){
+                number[i][j] = sc.nextInt();
+            }
+        }
+        //output
+        int x = sc.newInt();
+         for( int i=0;i<rows;i++){
+            for(int  j=0; j<cols; j++){
+                if( number[i][j]==x){
+                    System.out.println(number[i][j] );
+                }
+            }
+            System.out.println();
+        }
+
+
+
+
+
+    }
+    
     
 }
