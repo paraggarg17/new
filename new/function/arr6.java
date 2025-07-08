c22dc330d63a6beb7187c91f29e3720881145072
paraggarg17 <paraggarg1718@gmail.com>
2025-07-08 10:49:09 +0530

    ys7

--- new/function/arr6.java
+++ new/function/arr6.java
@@ -2,7 +2,7 @@ package function;
 
 public class arr6 {
     public static void maxsubarray(int number[]) {
-        int tp =0;
+      
         for(int i=0;i<number.length;i++){
             int start = i;
             for ( int j=i; j<number.length;j++){
@@ -10,13 +10,13 @@ public class arr6 {
                 for(int k=start;k<=end;k++){
                     
                     System.out.println(number[k]+" ");
-                    tp =number[k]+tp;
+                   
                 }
                 
                 System.out.println();
                 
             }
-            System.out.println(tp+"");
+            
            
         }
 
